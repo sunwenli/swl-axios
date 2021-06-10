e56b85075bfd845d01dae56bc82d6c3627b95009
wenli.sun <sunwenli0394@163.com>
2021-06-10 18:01:12 +0800

    将 response 的headers 解析为对象

--- src/xhr.ts
+++ src/xhr.ts
@@ -1,4 +1,5 @@
 import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
+import {parseHeaders} from './helpers/headers'
 export default function xhr(config: AxiosRequestConfig): AxiosPromise {
 
     return new Promise(resolve => {
@@ -33,7 +34,7 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
                 data: responseData,
                 status: request.status,
                 statusText: request.statusText,
-                headers: responseHeaders,
+                headers:parseHeaders(responseHeaders),
                 config,
                 request
             }
