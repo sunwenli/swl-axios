871aa7c2332ffbd22f9ee4abf95c70fd78ea4bd3
wenli.sun <sunwenli0394@163.com>
2021-06-16 16:35:54 +0800

    扩展接口，request 重载

---
+++ src/core/Axios.ts
@@ -0,0 +1,59 @@
+import { AxiosPromise, AxiosRequestConfig } from "../types";
+import dispatchRequest from "./dispatchRequest";
+
+export class Axios {
+    request(url?: any, config?: any): AxiosPromise {
+        if (typeof url === 'string') {
+            if (!config) {
+                config = {}
+            }
+            config.url = url
+        } else {
+            config = url
+        }
+        return dispatchRequest(config)
+    }
+
+    get(url: string, config?: AxiosRequestConfig): AxiosPromise {
+        return this._requestWithoutData(url, 'get', config)
+    }
+
+    delete(url: string, config?: AxiosRequestConfig): AxiosPromise {
+        return this._requestWithoutData(url, 'delete', config)
+    }
+
+    head(url: string, config?: AxiosRequestConfig): AxiosPromise {
+        return this._requestWithoutData(url, 'head', config)
+    }
+
+    options(url: string, config?: AxiosRequestConfig): AxiosPromise {
+        return this._requestWithoutData(url, 'options', config)
+    }
+
+    post(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise {
+        return this._requestWithData(url, 'post', data, config)
+    }
+
+    put(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise {
+        return this._requestWithData(url, 'put', data, config)
+    }
+
+    patch(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise {
+        return this._requestWithData(url, 'patch', data, config)
+    }
+
+    _requestWithData(url: string, method: string, data?: any, config?: AxiosRequestConfig): AxiosPromise {
+        return this.request(Object.assign(config || {}, {
+            method,
+            data,
+            url
+        }))
+    }
+
+    _requestWithoutData(url: string, method: string, config?: AxiosRequestConfig): AxiosPromise {
+        return this.request(Object.assign(config || {}, {
+            method,
+            url
+        }))
+    }
+}
--- src/core/xhr.ts
+++ src/core/xhr.ts
@@ -1,6 +1,6 @@
-import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
-import { parseHeaders } from './helpers/headers'
-import { createAxiosError } from './helpers/error'
+import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from '../types'
+import { parseHeaders } from '../helpers/headers'
+import { createAxiosError } from '../helpers/error'
 export default function xhr(config: AxiosRequestConfig): AxiosPromise {
 
     return new Promise((resolve, reject) => {
@@ -16,7 +16,7 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
             request.timeout = timeout
         }
 
-        request.open(method.toUpperCase(), url, true)
+        request.open(method.toUpperCase(), url!, true)
         // console.log('OPENED', request.readyState); // readyState 为 1 ，open()方法已出发，可以设置请求头（setRequestHeader），也可以发起请求(send)
 
         // request.onprogress = function handleProgress() {
